1b4a32f748c1f9e52455a8be7bd59fab2580f4b8
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-04 23:03:08 +0530

    Time: 593 ms (44.29%), Space: 92.2 MB (38.16%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j, int &val) {
+    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j, bool &flag) {
         if(i < 0 || j < 0 || i >= grid1.size() || j >= grid1[0].size() || !grid2[i][j])
             return;
         
         if(grid2[i][j] and !grid1[i][j]) {
-            val = 1;
+            flag = false;
             return;
         }
         
         grid2[i][j] = 0;
             
-        dfs(grid1, grid2, i - 1, j, val);
-        dfs(grid1, grid2, i + 1, j, val);
-        dfs(grid1, grid2, i, j - 1, val);
-        dfs(grid1, grid2, i, j + 1, val);
+        dfs(grid1, grid2, i - 1, j, flag);
+        dfs(grid1, grid2, i + 1, j, flag);
+        dfs(grid1, grid2, i, j - 1, flag);
+        dfs(grid1, grid2, i, j + 1, flag);
     
     }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
@@ -24,9 +24,10 @@ public:
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(grid2[i][j]) {
-                    int val = 0;
-                    dfs(grid1, grid2, i, j, val);
-                    ans += 1 - val;
+                    bool flag = true;
+                    dfs(grid1, grid2, i, j, flag);
+                    if(flag)
+                        ans++;
                 }
             }
         }
