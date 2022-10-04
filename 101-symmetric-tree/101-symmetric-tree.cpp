61137c7e6ac74d5268bfd7cd05fde2ce81563309
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-04 23:44:16 +0530

    Time: 7 ms (74.04%), Space: 16.5 MB (46.61%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -9,6 +9,42 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(root == nullptr) return true;
+        stack<TreeNode*> Stack;
+        Stack.push(root -> left);
+        Stack.push(root -> right);
+        while(!Stack.empty()) {
+            TreeNode* r = Stack.top();
+            Stack.pop();
+            TreeNode* l = Stack.top();
+            Stack.pop();
+            if(l == nullptr && r == nullptr) continue;
+            if(l == nullptr || r == nullptr || l -> val != r -> val) return false;
+            Stack.push(l -> left);
+            Stack.push(r -> right);
+            Stack.push(l -> right);
+            Stack.push(r -> left);
+        }
+        return true;
+    }
+};
+
+/*
 class Solution {
 public:
     bool solve(TreeNode* l, TreeNode* r) {
@@ -21,3 +57,4 @@ public:
         return solve(root -> left, root -> right);
     }
 };
+*/
