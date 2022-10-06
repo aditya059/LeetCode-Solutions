7ede69bed49453ae8ff2b4fa01f8ce6b7733e86b
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-06 23:32:37 +0530

    Time: 857 ms (9.46%), Space: 128 MB (93.61%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,22 +1,31 @@
 class TimeMap {
 public:
-    unordered_map<string, map<int, string>> Map;
+    unordered_map<string, vector<pair<int, string>>> Map;
     TimeMap() {
         
     }
     
     void set(string key, string value, int timestamp) {
-        Map[key][timestamp] = value;
+        Map[key].push_back({timestamp, value});
     }
     
     string get(string key, int timestamp) {
         if(Map.find(key) == Map.end()) return "";
         
-        auto iter = Map[key].upper_bound(timestamp);
-        
-        if(iter == Map[key].begin()) return "";
-        
-        return prev(iter) -> second;
+        int l = 0;
+        int h = Map[key].size() - 1;
+        int t = INT_MIN;
+        while(l <= h) {
+            int mid = l + (h - l) / 2;
+            if(Map[key][mid].first == timestamp) return Map[key][mid].second;
+            else if(Map[key][mid].first > timestamp) h = mid - 1;
+            else {
+                t = max(t, mid);
+                l = mid + 1;
+            }
+        }
+        if(t == INT_MIN) return "";
+        return Map[key][t].second;
     }
 };
 
