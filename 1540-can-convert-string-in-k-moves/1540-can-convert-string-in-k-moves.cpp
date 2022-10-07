fab61d2a3ef3dc8fdae23e0d264cc6c971fb9600
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-07 22:45:42 +0530

    Time: 176 ms (32.19%), Space: 17.8 MB (39.04%) - LeetHub

--- 1540-can-convert-string-in-k-moves/1540-can-convert-string-in-k-moves.cpp
+++ 1540-can-convert-string-in-k-moves/1540-can-convert-string-in-k-moves.cpp
@@ -4,10 +4,10 @@ public:
         if(s.length() != t.length()) return false;
         int Map[26] = {0};
         for(int i = 0; i < s.length(); i++) {
-            Map[(26 + t[i] - s[i]) % 26]++;
-        }
-        for(int i = 1; i <= 25; i++) {
-            if((Map[i] - 1) * 26 + i > k) return false;
+            int val = (26 + t[i] - s[i]) % 26;
+            if(!val) continue;
+            if(val + 26 * Map[val] > k) return false;
+            Map[val]++;
         }
         return true;
     }
