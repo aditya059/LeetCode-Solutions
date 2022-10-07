94356247f9f241c174c0a825e61777d348a31207
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-07 22:38:39 +0530

    Time: 258 ms (17.81%), Space: 17.9 MB (39.04%) - LeetHub

--- 1540-can-convert-string-in-k-moves/1540-can-convert-string-in-k-moves.cpp
+++ 1540-can-convert-string-in-k-moves/1540-can-convert-string-in-k-moves.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canConvertString(string s, string t, int k) {
         if(s.length() != t.length()) return false;
-        unordered_map<int, int> Map;
+        vector<int> Map(26, 0);
         for(int i = 0; i < s.length(); i++) {
             Map[(26 + t[i] - s[i]) % 26]++;
         }
