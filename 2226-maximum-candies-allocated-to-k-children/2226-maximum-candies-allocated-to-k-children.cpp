d28f30a2d8a8616a413165332a482b702d664136
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-13 03:05:31 +0530

    Time: 565 ms (16.14%), Space: 84.4 MB (16.23%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-    long long solve(vector<int> &nums, long long &val) {
-        long long sum = 0;
-        for(int num: nums) {
-            sum += num / val;
-        }
-        return sum;
-    }
     int maximumCandies(vector<int>& candies, long long k) {
         long long sum = accumulate(candies.begin(), candies.end(), 0L);
         if(sum < k) return 0;
@@ -15,7 +8,11 @@ public:
         long long ans = INT_MIN;
         while(low <= high) {
             long long mid = low + (high - low) / 2;
-            if(solve(candies, mid) >= k) {
+            long long sum = 0;
+            for(int num: candies) {
+                sum += num / mid;
+            }
+            if(sum >= k) {
                 ans = max(ans, mid);
                 low = mid + 1;
             }
