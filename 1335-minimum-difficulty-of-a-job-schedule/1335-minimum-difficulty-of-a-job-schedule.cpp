87f42bd8b3858ab2878b32ded7d0a5828c994251
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-17 00:21:31 +0530

    Time: 265 ms (18.15%), Space: 7.6 MB (68.79%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
+    int solve(vector<int>& jobDifficulty, int d, vector<vector<int>> &DP, int index) {
+        if(d == 1) {
+            int mx = 0;
+            for(int i = index; i < jobDifficulty.size(); i++) 
+                mx = max(mx, jobDifficulty[i]);
+            return mx;
+        }
+        if(DP[d][index] != -1) return DP[d][index];
+        int mx = 0;
+        int ans = INT_MAX;
+        for(int i = index; i < jobDifficulty.size() - d + 1; i++) {
+            mx = max(mx, jobDifficulty[i]);
+            ans = min(ans, mx + solve(jobDifficulty, d - 1, DP, i + 1));
+        }
+        return DP[d][index] = ans;
+    }
     int minDifficulty(vector<int>& jobDifficulty, int d) {
         int n = jobDifficulty.size();
         if(d > n) return -1;
-        vector<int> DP(n + 1, INT_MAX / 100);
-        DP[n] = 0;
-        for(int i = 1; i <= d; i++) {
-            for(int j = 0; j <= n - i; j++) {
-                int temp = INT_MAX / 100;
-                int p = 0;
-                for(int k = j; k <= n - i; k++) {
-                    p = max(p, jobDifficulty[k]);
-                    temp = min(temp, p + DP[k + 1]);
-                }
-                DP[j] = temp;
-            }
-        }
-        return DP[0];
+        vector<vector<int>> DP(d + 1, vector<int>(n, -1));
+        return solve(jobDifficulty, d, DP, 0);
     }
 };
