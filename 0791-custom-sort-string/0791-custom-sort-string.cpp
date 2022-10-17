a683bdb39f76872dca92b780a7aafdf208f77cf5
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-17 20:13:23 +0530

    Time: 0 ms (100.00%), Space: 6 MB (96.76%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -9,3 +9,13 @@ public:
         return s;
     }
 };
+
+/*
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        sort(s.begin(), s.end(), [&](char &a, char &b) {return order.find(a) < order.find(b);});
+        return s;
+    }
+};
+*/
