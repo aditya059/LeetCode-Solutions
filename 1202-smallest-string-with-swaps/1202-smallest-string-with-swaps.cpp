d4733b7d9ce7ced8bd72cec89b86e7d648eff8a2
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-17 22:08:10 +0530

    Time: 529 ms (16.00%), Space: 72.8 MB (23.95%) - LeetHub

---
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void dfs(int curr, string &s, vector<int> adjList[], vector<bool> &visited, vector<int> &indexes, string &values) {
+        if(visited[curr]) {
+            return;
+        }
+        indexes.push_back(curr);
+        values += s[curr];
+        visited[curr] = true;
+        for(int next: adjList[curr]) {
+            dfs(next, s, adjList, visited, indexes, values);
+        }
+    }
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        vector<bool> visited(s.length());
+        vector<int> adjList[s.length()];
+        for(vector<int> pair: pairs) {
+            adjList[pair[0]].push_back(pair[1]);
+            adjList[pair[1]].push_back(pair[0]);
+        }
+        for(int i = 0; i < s.length(); i++) {
+            vector<int> indexes;
+            string values = "";
+            dfs(i, s, adjList, visited, indexes, values);
+            sort(indexes.begin(), indexes.end());
+            sort(values.begin(), values.end());
+            for(int i = 0; i < indexes.size(); i++) {
+                s[indexes[i]] = values[i];
+            }
+        }
+        return s;
+    }
+};
