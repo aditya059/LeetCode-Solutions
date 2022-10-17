93c19c2fabf491ed962728d71f855b878ed45790
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-17 20:12:42 +0530

    Time: 0 ms (100.00%), Space: 6 MB (99.87%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        sort(s.begin(), s.end(), [&](char &a, char &b) {return order.find(a) < order.find(b);});
+        vector<int> pos(26, INT_MAX);
+        for(int i = 0; i < order.length(); i++) {
+            pos[order[i] - 'a'] = min(pos[order[i] - 'a'], i);
+        }
+        sort(s.begin(), s.end(), [&](char &a, char &b) {return pos[a - 'a'] < pos[b - 'a'];});
         return s;
     }
 };
