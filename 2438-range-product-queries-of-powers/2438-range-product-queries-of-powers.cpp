fd9ef3e2a007f69b8bb3a8fffc7610335d87b5f8
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-29 01:05:37 +0530

    Time: 520 ms (86.04%), Space: 160.6 MB (30.28%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
@@ -7,12 +7,19 @@ public:
             if(n & power)
                 powers.push_back(power);
         }
+        int DP[32][32] = {0};
         const int MOD = 1e9 + 7;
         vector<int> ans;
         for(vector<int> query: queries) {
             long prod = 1;
-            for(int i = query[0]; i <= query[1]; i++) {
-                prod = (prod * powers[i]) % MOD;
+            if(!DP[query[0]][query[1]]) {
+                
+                for(int i = query[0]; i <= query[1]; i++) {
+                    prod = (prod * powers[i]) % MOD;
+                }
+            }
+            else {
+                prod = DP[query[0]][query[1]];
             }
             ans.push_back(prod);
         }
