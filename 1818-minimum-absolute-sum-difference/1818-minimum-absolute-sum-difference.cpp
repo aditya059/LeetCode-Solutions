fc25c7a7b0a70f6dd3797bb9e1c0590d0e839912
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-29 13:32:29 +0530

    Time: 680 ms (5.05%), Space: 74.7 MB (16.85%) - LeetHub

---
+++ 1818-minimum-absolute-sum-difference/1818-minimum-absolute-sum-difference.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minAbsoluteSumDiff(vector<int>& nums1, vector<int>& nums2) {
+        const int MOD = 1e9 + 7;
+        int n = nums1.size();
+        long totalDiff = 0;
+        set<int> Set;
+        for(int i = 0; i < n; i++) {
+            totalDiff += abs(nums1[i] - nums2[i]);
+            Set.insert(nums1[i]);
+        }
+        long ans = totalDiff;
+        for(int i = 0; i < n; i++) {
+            auto iter = Set.upper_bound(nums2[i]);
+            int mn = INT_MAX;
+            if(iter != Set.end()) 
+                mn = min(mn, abs(nums2[i] - *iter));
+            if(iter != Set.begin())
+                mn = min(mn, abs(nums2[i] - (*(--iter))));
+            ans = min(ans, totalDiff - abs(nums1[i] - nums2[i]) + mn);
+        }
+        return ans % MOD;
+    }
+};
