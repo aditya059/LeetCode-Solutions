da2fcf95003722cead95b8b7047caacf4d3171c7
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-29 23:48:19 +0530

    Time: 1233 ms (20.42%), Space: 10.5 MB (19.05%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        vector<vector<int>> DP(n + 2, vector<int> (n + 2, 0));
+        for(int left = n; left >= 1; left--) {
+            for(int right = 1; right <= n; right++) {
+                if(left > right) continue;;
+                for(int i = left; i <= right; i++) {
+                    int coins = nums[left - 1] * nums[i] * nums[right + 1] + DP[left][i - 1] + DP[i + 1][right];
+                    DP[left][right] = max(DP[left][right], coins);
+                }
+            }
+        }
+        return DP[1][n];
+    }
+};
+
+
+/*
+class Solution {
+public:
     int solve(vector<int> &nums, int left, int right, vector<vector<int>> &memo) {
         if(left > right) return 0;
         if(memo[left][right] != -1) return memo[left][right];
@@ -18,3 +40,4 @@ public:
         return solve(nums, 1, n, memo);
     }
 };
+*/
