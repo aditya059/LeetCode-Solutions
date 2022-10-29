c92c60cedb75ac5f7cd69cac0833ad1486a090ce
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-29 18:57:51 +0530

    Time: 7 ms (88.34%), Space: 5.9 MB (88.03%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    void dfs(string &tiles, int index, string &temp, set<string> &Set, vector<bool> &index_used) {
-        for(int i = 0; i < tiles.size(); i++) {
-            if(!index_used[i]) {
-                temp.push_back(tiles[i]);
-                Set.insert(temp);
-                index_used[i] = true;
-                dfs(tiles, i, temp, Set, index_used);
-                index_used[i] = false;
-                temp.pop_back();
-            }
+    void dfs(int count[], int &ans) {
+        for(int i = 0; i < 26; i++) {
+            if(count[i] > 0) {
+                ans++;
+                count[i]--;
+                dfs(count, ans);
+                count[i]++;
+            } 
         }
     }
     int numTilePossibilities(string tiles) {
-        set<string> Set;
-        string temp = "";
-        vector<bool> index_used(tiles.size());
-        dfs(tiles, 0, temp, Set, index_used);
-        return Set.size();
+        int count[26] = {0};
+        int ans = 0;
+        for(char ch: tiles) count[ch - 'A']++;
+        dfs(count, ans);
+        return ans;
     }
 };
