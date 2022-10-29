c227cddeb3d8eb97244fabcd316bfd669abf65b8
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-29 17:19:02 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (19.63%) - LeetHub

--- 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cpp
+++ 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     bool queryString(string s, int n) {
         set<int> Set;
+        int len = max(1, (int)ceil(log2(n)));
         for(int i = 0; i < s.length(); i++) {
             int val = 0;
-            for(int j = i; j < min((int)s.length(), i + 31); j++) {
+            for(int j = i; j < min((int)s.length(), i + len); j++) {
                 val = val * 2 + (s[j] - '0');
                 if(val > 0 && val <= n)
                     Set.insert(val);
