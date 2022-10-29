0f0da2f8e8a60dfda70d4f17cfcc297ae86526dc
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-29 20:43:30 +0530

    Time: 369 ms (24.95%), Space: 94.3 MB (82.31%) - LeetHub

--- 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.cpp
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.cpp
@@ -3,16 +3,15 @@ public:
     int waysToMakeFair(vector<int>& nums) {
         int n = nums.size();
         int ans = 0;
-        int left[2] = {0};
-        int right[2] = {0};
+        int sum[2] = {0};
         for(int i = 0; i < n; i++) {
-            right[i % 2] += nums[i];
+            sum[i % 2] += nums[i];
         }
         for(int i = 0; i < n; i++) {
-            right[i % 2] -= nums[i];
-            if(left[0] + right[1] == left[1] + right[0])
+            sum[i % 2] -= nums[i];
+            if(sum[0] == sum[1])
                 ans++;
-            left[i % 2] += nums[i];
+            sum[1 - (i % 2)] += nums[i];
         }
         return ans;
     }
