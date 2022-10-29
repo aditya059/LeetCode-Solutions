9fc6afbb84045897feeb869ad78fcf107b91a74f
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-29 19:04:05 +0530

    Time: 8 ms (84.14%), Space: 6 MB (71.32%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -18,3 +18,30 @@ public:
         return ans;
     }
 };
+
+/*
+
+class Solution {
+public:
+    void dfs(string &tiles, int index, string &temp, set<string> &Set, vector<bool> &index_used) {
+        for(int i = 0; i < tiles.size(); i++) {
+            if(!index_used[i]) {
+                temp.push_back(tiles[i]);
+                Set.insert(temp);
+                index_used[i] = true;
+                dfs(tiles, i, temp, Set, index_used);
+                index_used[i] = false;
+                temp.pop_back();
+            }
+        }
+    }
+    int numTilePossibilities(string tiles) {
+        set<string> Set;
+        string temp = "";
+        vector<bool> index_used(tiles.size());
+        dfs(tiles, 0, temp, Set, index_used);
+        return Set.size();
+    }
+};
+
+*/
