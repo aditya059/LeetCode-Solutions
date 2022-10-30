3d1b44f602380f13b3d1b004ef489e17ef8c932d
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-30 17:19:14 +0530

    Time: 878 ms (83.33%), Space: 126.3 MB (41.11%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,4 +1,48 @@
-// Dijkstra's - O(m * n * log (m * n))
+// 0-1 BFS
+class Solution {
+public:
+    int minimumObstacles(vector<vector<int>>& grid) {
+        int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; 
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n));
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        deque<pair<int, int>> PQ;
+        PQ.push_back({0, 0});
+        visited[0][0] = true;
+        dist[0][0] = 0;
+
+        while(!PQ.empty()) {
+            pair<int, int> curr = PQ.front();
+            PQ.pop_front();
+            
+            int ux = curr.first;
+            int uy = curr.second;
+            
+            for(auto dir: dirs) {
+                int vx = ux + dir[0];
+                int vy = uy + dir[1];
+
+                if(vx < 0 || vy < 0 || vx >= m || vy >= n) 
+                    continue;
+                
+                if(!visited[vx][vy]) {
+                    visited[vx][vy] = true;
+                    
+                    dist[vx][vy] = dist[ux][uy] + grid[vx][vy];
+                    if(grid[vx][vy])
+                        PQ.push_back({vx, vy});
+                    else
+                        PQ.push_front({vx, vy});
+                }
+            }
+        }
+        return dist[m - 1][n - 1];
+    }
+};
+
+/*
+// Dijkstra's
 class Solution {
 public:
     int minimumObstacles(vector<vector<int>>& grid) {
@@ -37,3 +81,4 @@ public:
         return dist[m - 1][n - 1];
     }
 };
+*/
