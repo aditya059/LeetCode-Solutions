71e9098851cbacdb3bc664c1288ef09ff4b258c9
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-30 17:13:30 +0530

    Time: 2060 ms (18.87%), Space: 213.2 MB (15.18%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -4,19 +4,15 @@ public:
         int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; 
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n));
         vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-        deque<pair<int, int>> PQ;
-        PQ.push_back({0, 0});
-        visited[0][0] = true;
+        set<pair<int, pair<int,int>>> PQ;
+        PQ.insert({0, {0, 0}});
         dist[0][0] = 0;
-
         while(!PQ.empty()) {
-            pair<int, int> curr = PQ.front();
-            PQ.pop_front();
-            
-            int ux = curr.first;
-            int uy = curr.second;
+            pair<int, pair<int,int>> curr = *PQ.begin();
+            PQ.erase(PQ.begin());
+            int ux = curr.second.first;
+            int uy = curr.second.second;
             
             for(auto dir: dirs) {
                 int vx = ux + dir[0];
@@ -25,15 +21,14 @@ public:
                 if(vx < 0 || vy < 0 || vx >= m || vy >= n) 
                     continue;
                 
-                if(!visited[vx][vy]) {
-                    visited[vx][vy] = true;
-                    
-                    dist[vx][vy] = dist[ux][uy] + grid[vx][vy];
-                    if(grid[vx][vy])
-                        PQ.push_back({vx, vy});
-                    else
-                        PQ.push_front({vx, vy});
+                int cost = dist[ux][uy] + grid[vx][vy];
+                
+                if(cost < dist[vx][vy]) {
+                    PQ.erase({dist[vx][vy], {vx, vy}});
+                    dist[vx][vy] = cost;
+                    PQ.insert({dist[vx][vy], {vx, vy}});
                 }
+
             }
         }
         return dist[m - 1][n - 1];
