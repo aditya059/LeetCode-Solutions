c99591168c0670c50d1f3346913445d7ae7836f1
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-30 17:08:40 +0530

    Time: 705 ms (92.59%), Space: 126.4 MB (40.49%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -4,15 +4,19 @@ public:
         int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; 
         int m = grid.size();
         int n = grid[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n));
         vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-        set<pair<int, pair<int,int>>> PQ;
-        PQ.insert({0, {0, 0}});
+        deque<pair<int, int>> PQ;
+        PQ.push_back({0, 0});
+        visited[0][0] = true;
         dist[0][0] = 0;
+
         while(!PQ.empty()) {
-            pair<int, pair<int,int>> curr = *PQ.begin();
-            PQ.erase(PQ.begin());
-            int ux = curr.second.first;
-            int uy = curr.second.second;
+            pair<int, int> curr = PQ.front();
+            PQ.pop_front();
+            
+            int ux = curr.first;
+            int uy = curr.second;
             
             for(auto dir: dirs) {
                 int vx = ux + dir[0];
@@ -21,16 +25,15 @@ public:
                 if(vx < 0 || vy < 0 || vx >= m || vy >= n) 
                     continue;
                 
-                int cost = dist[ux][uy] + grid[vx][vy];
-                
-                if(cost < dist[vx][vy]) {
-                    auto iter = PQ.find({dist[vx][vy], {vx, vy}});
-                    if(iter != PQ.end())
-                        PQ.erase(iter);
-                    dist[vx][vy] = cost;
-                    PQ.insert({dist[vx][vy], {vx, vy}});
+                if(!visited[vx][vy]) {
+                    visited[vx][vy] = true;
+                    
+                    dist[vx][vy] = dist[ux][uy] + grid[vx][vy];
+                    if(grid[vx][vy])
+                        PQ.push_back({vx, vy});
+                    else
+                        PQ.push_front({vx, vy});
                 }
-
             }
         }
         return dist[m - 1][n - 1];
