8c9b87db377ca5d606e8adad272151ec0ce2feaf
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-30 21:25:23 +0530

    Time: 24 ms (97.20%), Space: 22.4 MB (10.64%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,19 +5,15 @@ public:
         int h = nums.size() - 1;
         while(l < h) {
             int m = l + (h - l) / 2;
-            if(m == 0 || nums[m] != nums[m - 1] && nums[m] != nums[m + 1]) return nums[m];
-            if((m - l + 1) % 2) {
-                if(nums[m] == nums[m - 1]) h = m - 2;
-                else l = m + 2;
-            }
-            else {
-                if(nums[m] != nums[m - 1]) h = m - 1;
-                else l = m + 1;
-            }
+            if((m % 2 == 0 && nums[m] == nums[m + 1]) || (m % 2 == 1 && nums[m] == nums[m - 1]))
+                l = m + 1;
+            else
+                h = m;
         }
         return nums[l];
     }
 };
+
 
 /*
 class Solution {
