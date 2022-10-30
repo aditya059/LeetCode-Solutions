fd1fc5429fbcaf39d9b4bb0ed366076a5d4d44ab
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-10-30 16:37:16 +0530

    Time: 1142 ms (61.35%), Space: 213.2 MB (14.44%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -4,36 +4,33 @@ public:
         int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; 
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> visited(m, vector<int>(n, INT_MAX));
-        deque<vector<int>> Queue;
-        Queue.push_front({0, 0, 0});
-        visited[0][0] = true;
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        set<pair<int, pair<int,int>>> PQ;
+        PQ.insert({0, {0, 0}});
+        dist[0][0] = 0;
+        while(!PQ.empty()) {
+            pair<int, pair<int,int>> curr = *PQ.begin();
+            PQ.erase(PQ.begin());
+            int ux = curr.second.first;
+            int uy = curr.second.second;
+            
+            for(auto dir: dirs) {
+                int vx = ux + dir[0];
+                int vy = uy + dir[1];
 
-        while(!Queue.empty()) {
-            int queueLen = Queue.size();
-            for(int i = 0; i < queueLen; i++) {
-                vector<int> curr = Queue.front();
-                Queue.pop_front();
+                if(vx < 0 || vy < 0 || vx >= m || vy >= n) 
+                    continue;
                 
-                for(auto dir: dirs) {
-                    int x = curr[0] + dir[0];
-                    int y = curr[1] + dir[1];
-                    
-                    if(x < 0 || y < 0 || x >= m || y >= n) 
-                        continue;
-                    
-                    int temp = curr[2] + grid[x][y];
-                    if(temp < visited[x][y])
-                    {
-                        visited[x][y] = temp;
-                        if(grid[x][y])
-                            Queue.push_back({x, y, temp});
-                        else
-                            Queue.push_front({x, y, temp});
-                    }
+                int cost = dist[ux][uy] + grid[vx][vy];
+                
+                if(cost < dist[vx][vy]) {
+                    PQ.erase({dist[vx][vy], {vx, vy}});
+                    dist[vx][vy] = cost;
+                    PQ.insert({dist[vx][vy], {vx, vy}});
                 }
+
             }
         }
-        return visited[m - 1][n - 1];
+        return dist[m - 1][n - 1];
     }
 };
