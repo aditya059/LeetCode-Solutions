fe63b20c6352dff9953ba032c5b90a9a8bac4a4d
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-11-01 14:18:17 +0530

    Time: 28 ms (89.01%), Space: 14.3 MB (8.82%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    int dfs(vector<vector<int>> &grid, int row, int col, vector<vector<int>> &memo) {
+        // Base Case
+        if(row == grid.size()) return col;
+        
+        if(memo[row][col] != -1) return memo[row][col];
+        
+        int nextcol = col + grid[row][col];
+        
+        if(nextcol == -1 || nextcol == grid[0].size() || grid[row][col] != grid[row][nextcol])
+            return -1;
+        
+        return memo[row][col] = dfs(grid, row + 1, nextcol, memo);
+    }
+    vector<int> findBall(vector<vector<int>>& grid) {
+        vector<int> ans(grid[0].size());
+        vector<vector<int>> memo(grid.size(), vector<int>(grid[0].size(), -1));
+        for(int col = 0; col < grid[0].size(); col++) {
+            ans[col] = dfs(grid, 0, col, memo);
+        }
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
     int dfs(vector<vector<int>> &grid, int row, int col) {
         // Base Case
         if(row == grid.size()) return col;
@@ -19,3 +45,4 @@ public:
         return ans;
     }
 };
+*/
