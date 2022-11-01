d2658acc2b86c0fc1739c9be2a71369f93bf0816
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-11-01 14:36:39 +0530

    Time: 61 ms (34.61%), Space: 14.3 MB (5.40%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    vector<int> findBall(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> ans(n);
+        vector<vector<int>> DP(m + 1, vector<int>(n));
+        iota(DP[m].begin(), DP[m].end(), 0);
+        for(int row = m - 1; row >= 0; row--) {
+            for(int col = 0; col < n; col++) {
+                
+                int nextcol = col + grid[row][col];
+
+                if(nextcol == -1 || nextcol == grid[0].size() || grid[row][col] != grid[row][nextcol])
+                    DP[row][col] = -1;
+                else
+                    DP[row][col] = DP[row + 1][nextcol];
+            
+            }
+        }
+        return vector<int>(DP[0].begin(), DP[0].end());
+    }
+};
+
+/*
+class Solution {
+public:
     int dfs(vector<vector<int>> &grid, int row, int col, vector<vector<int>> &memo) {
         // Base Case
         if(row == grid.size()) return col;
@@ -22,6 +47,7 @@ public:
         return ans;
     }
 };
+*/
 
 /*
 class Solution {
