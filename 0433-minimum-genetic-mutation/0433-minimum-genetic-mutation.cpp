f2be5e060059683aad4fd1f88c396a64d9ccec5c
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-11-02 13:40:21 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (42.79%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -2,6 +2,42 @@ class Solution {
 public:
     int minMutation(string start, string end, vector<string>& bank) {
         char allowedChar[4] = {'A', 'C', 'G', 'T'};
+        sort(bank.begin(), bank.end());
+        vector<bool> visited(bank.size());
+        queue<string> Queue;
+        Queue.push(start);
+        int ans = -1;
+        while(!Queue.empty()) {
+            int sz = Queue.size();
+            ans++;
+            for(int i = 0; i < sz; i++) {
+                string curr = Queue.front();
+                Queue.pop();
+                if(curr == end) return ans;
+                for(int j = 0; j < 8; j++) {
+                    string next = curr;
+                    for(char ch: allowedChar) {
+                        next[j] = ch;
+                        if(binary_search(bank.begin(), bank.end(), next)) {
+                            int index = lower_bound(bank.begin(), bank.end(), next) - bank.begin();
+                            if(!visited[index]) {
+                                Queue.push(next);
+                                visited[index] = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+/*
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        char allowedChar[4] = {'A', 'C', 'G', 'T'};
         set<string> Set(bank.begin(), bank.end());
         queue<string> Queue;
         Queue.push(start);
@@ -29,3 +65,4 @@ public:
         return -1;
     }
 };
+*/
