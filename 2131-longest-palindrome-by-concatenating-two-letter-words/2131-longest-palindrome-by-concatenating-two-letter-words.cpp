ca58dcd2107aa3cc31271afa10a7f73db8193e5c
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-11-04 02:01:45 +0530

    Time: 373 ms (94.32%), Space: 167.5 MB (97.94%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -4,12 +4,14 @@ public:
         int ans = 0;
         int Map[26][26] = {0};
         for(string word: words) {
-            if(Map[word[1] - 'a'][word[0] - 'a'] != 0) {
+            short i = word[0] - 'a';
+            short j = word[1] - 'a';
+            if(Map[j][i] != 0) {
                 ans += 4;
-                Map[word[1] - 'a'][word[0] - 'a']--;
-                Map[word[0] - 'a'][word[1] - 'a']--;
+                Map[j][i]--;
+                Map[i][j]--;
             }
-            Map[word[0] - 'a'][word[1] - 'a']++;
+            Map[i][j]++;
         }
         for(int i = 0; i < 26; i++) {
             if(Map[i][i]) return ans + 2;
