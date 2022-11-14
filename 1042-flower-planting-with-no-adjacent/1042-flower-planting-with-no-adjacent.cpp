f560cbe9f3a86a2b8723b52509fe29c5a602e089
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-11-14 23:15:37 +0530

    Time: 179 ms (85.18%), Space: 44.4 MB (57.87%) - LeetHub

--- 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
@@ -6,6 +6,41 @@ public:
         for(int next: adjList[curr]) {
             dfs(next, adjList, visited, ans);
         }
+        int val = 0;
+        for(int next: adjList[curr]) {
+            val |= 1 << ans[next];
+        }
+        for(int i = 1; i <= 4; i++) {
+            if((val & (1 << i)) == 0) {
+                ans[curr] = i;
+                break;
+            }
+        }
+    }
+    vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
+        vector<int> ans(n);
+        vector<int> adjList[n];
+        vector<bool> visited(n);
+        for(vector<int> edge: paths) {
+            adjList[edge[1] - 1].push_back(edge[0] - 1);
+            adjList[edge[0] - 1].push_back(edge[1] - 1);
+        }
+        for(int i = 0; i < n; i++) {
+            dfs(i, adjList, visited, ans);
+        }
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    void dfs(int curr, vector<int> adjList[], vector<bool> &visited, vector<int> &ans) {
+        if(visited[curr]) return;
+        visited[curr] = true;
+        for(int next: adjList[curr]) {
+            dfs(next, adjList, visited, ans);
+        }
         set<int> Set({1, 2, 3, 4});
         for(int next: adjList[curr]) {
             Set.erase(ans[next]);
@@ -26,3 +61,4 @@ public:
         return ans;
     }
 };
+*/
