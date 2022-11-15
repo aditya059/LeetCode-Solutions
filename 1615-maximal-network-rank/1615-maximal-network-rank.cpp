e1ee3420081338105395b3ad1c75e0242b196f44
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-11-15 23:00:52 +0530

    Time: 216 ms (45.38%), Space: 36 MB (51.57%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         vector<int> adjList[n];
-        map<pair<int, int>, int> Map;
+        vector<vector<int>> Map(n, vector<int>(n));
         for(vector<int> edge: roads) {
             adjList[edge[0]].push_back(edge[1]);
             adjList[edge[1]].push_back(edge[0]);
-            Map[{edge[0], edge[1]}] = 1;
-            Map[{edge[1], edge[0]}] = 1;
+            Map[edge[0]][edge[1]] = 1;
+            Map[edge[1]][edge[0]] = 1;
         }
         int ans = 0;
         for(int i = 0; i < n; i++) {
             for(int j = i + 1; j < n; j++) {
-                ans = max(ans, (int)adjList[i].size() + (int)adjList[j].size() - Map[{i, j}]);
+                ans = max(ans, (int)adjList[i].size() + (int)adjList[j].size() - Map[i][j]);
             }
         }
         return ans;
