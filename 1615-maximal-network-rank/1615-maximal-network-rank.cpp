a05a6311a70777ef04a650b5e601a34caf81b16e
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-11-16 00:36:22 +0530

    Time: 115 ms (87.38%), Space: 33 MB (65.02%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        vector<int> degree(n), index(n);
+        iota(index.begin(), index.end(), 0);
+        vector<vector<int>> Map(n, vector<int>(n));
+        for(vector<int> edge: roads) {
+            degree[edge[0]]++;
+            degree[edge[1]]++;
+            Map[edge[0]][edge[1]] = 1;
+            Map[edge[1]][edge[0]] = 1;
+        }
+        sort(index.begin(), index.end(), [&](int i, int j) {return degree[i] > degree[j]; });
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                ans = max(ans, degree[index[i]] + degree[index[j]] - Map[index[i]][index[j]]);
+            }
+        }
+        return ans;
+    }
+};
+/*
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         vector<int> degree(n);
         vector<vector<int>> Map(n, vector<int>(n));
         for(vector<int> edge: roads) {
@@ -18,3 +41,4 @@ public:
         return ans;
     }
 };
+*/
