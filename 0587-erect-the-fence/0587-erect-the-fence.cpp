01d92da4aeecf977ee2964249673cf0f40045c10
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-11-20 01:27:15 +0530

    Time: 82 ms (80.47%), Space: 21.7 MB (58.59%) - LeetHub

--- 0587-erect-the-fence/0587-erect-the-fence.cpp
+++ 0587-erect-the-fence/0587-erect-the-fence.cpp
@@ -31,6 +31,42 @@ public:
 /*
 class Solution {
 public:
+    static bool comp(vector<int> &A, vector<int> &B) {
+        return A[0] < B[0] || (A[0] == B[0] && A[1] < B[1]);
+    }
+    int cross_product(vector<int> &O, vector<int> &A, vector<int> &B) {
+        return (A[0] - O[0]) * (B[1] - O[1]) - (A[1] - O[1]) * (B[0] - O[0]);
+    }
+    
+    vector<vector<int>> outerTrees(vector<vector<int>>& trees) {
+        int n = trees.size();
+        sort(trees.begin(), trees.end(), comp);
+        vector<vector<int>> ans(n << 1);
+        int index = 0;
+        for(int i = 0; i < n; i++) {
+            while(index >= 2 && cross_product(ans[index - 2], ans[index - 1], trees[i]) < 0)
+                index--;
+            ans[index++] = trees[i];
+        }
+        for(int i = n - 1; i >= 0; i--) {
+            while(index >= 2 && cross_product(ans[index - 2], ans[index - 1], trees[i]) < 0)
+                index--;
+            ans[index++] = trees[i];
+        }
+        ans.resize(index);
+        sort(ans.begin(), ans.end(), comp);
+        int k = 0;
+        for(int i = 1; i < index; i++) {
+            if(ans[i][0] == ans[k][0] && ans[i][1] == ans[k][1]) continue;
+            ans[++k] = ans[i];
+        }
+        ans.resize(k + 1);
+        return ans;                                                           
+    }
+};
+
+class Solution {
+public:
     int cross_product(vector<int> &O, vector<int> &A, vector<int> &B) {
         return (A[0] - O[0]) * (B[1] - O[1]) - (A[1] - O[1]) * (B[0] - O[0]);
     }
