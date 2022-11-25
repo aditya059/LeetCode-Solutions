804fd6f4169cbdc2f6d80e232af9f87892157459
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-11-26 02:58:55 +0530

    Time: 120 ms (85.76%), Space: 41.4 MB (75.54%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,50 +1,27 @@
 class Solution {
 public:
-    int sumSubarrayMins(vector<int>& arr) {
-        const int MOD = 1e9 + 7;
-        long ans = 0;
-        int n = arr.size();
-        stack<int> Stack;
-        for(int i = 0; i <= n; i++) {
-            while(!Stack.empty() && (i == n || arr[i] <= arr[Stack.top()])) {
-                int middle = Stack.top();
-                Stack.pop();
-                int right = i;
-                int left = Stack.empty()? -1: Stack.top();
-                long count = (middle - left) * (right - middle);
-                ans = (ans + (arr[middle] * count % MOD) % MOD) % MOD;
-            }
-            Stack.push(i);
-        }
-        return ans;
-    }
+	int sumSubarrayMins(vector<int>& arr) {
+		const int MOD = 1e9 + 7;
+		long ans = 0;
+		int n = arr.size();
+		stack<int> Stack;
+		vector<int> left(n);
+		for(int i = 0; i < n; i++) {
+			while(!Stack.empty() && arr[i] <= arr[Stack.top()]) {
+				Stack.pop();
+			}
+			left[i] = Stack.empty()? i + 1: i - Stack.top();
+			Stack.push(i);
+		}
+		while(!Stack.empty()) Stack.pop();
+		for(int i = n - 1; i >= 0; i--) {
+			while(!Stack.empty() && arr[i] < arr[Stack.top()]) {
+				Stack.pop();
+			}
+			int right = Stack.empty()? n - i: Stack.top() - i;
+			ans = (ans + (((left[i] * 1L * right) % MOD) * arr[i]) % MOD) % MOD;
+			Stack.push(i);
+		}
+		return ans;
+	}
 };
-/*
-class Solution {
-public:
-    int sumSubarrayMins(vector<int>& arr) {
-        const int MOD = 1e9 + 7;
-        long ans = 0;
-        int n = arr.size();
-        stack<int> Stack;
-        vector<int> DP(n);
-        for(int i = 0; i < n; i++) {
-            while(!Stack.empty() && arr[i] <= arr[Stack.top()]) {
-                Stack.pop();
-            }
-            DP[i] = Stack.empty()? i + 1: i - Stack.top();
-            Stack.push(i);
-        }
-        while(!Stack.empty()) Stack.pop();
-        for(int i = n - 1; i >= 0; i--) {
-            while(!Stack.empty() && arr[i] < arr[Stack.top()]) {
-                Stack.pop();
-            }
-            long right = Stack.empty()? n - i: Stack.top() - i;
-            ans = (ans + (((DP[i] * right) % MOD) * arr[i]) % MOD) % MOD;
-            Stack.push(i);
-        }
-        return ans;
-    }
-};
-*/
