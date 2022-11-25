29ea71036684d980c560be8be14d901431aad00b
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-11-25 13:43:45 +0530

    Time: 125 ms (96.00%), Space: 29.8 MB (96.19%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -3,20 +3,20 @@ public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
         int m = maze.size();
         int n = maze[0].size();
-        vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        queue<vector<int>> Queue;
-        Queue.push(entrance);
+        vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        queue<pair<int, int>> Queue;
+        Queue.push({entrance[0], entrance[1]});
         maze[entrance[0]][entrance[1]] = '+';
         int ans = -1;
         while(!Queue.empty()) {
             int sz = Queue.size();
             ans++;
             for(int i = 0; i < sz; i++) {
-                vector<int> curr = Queue.front();
+                pair<int, int> curr = Queue.front();
                 Queue.pop();
-                for(vector<int> dir: dirs) {
-                    int x = curr[0] + dir[0];
-                    int y = curr[1] + dir[1];
+                for(pair<int, int> dir: dirs) {
+                    int x = curr.first + dir.first;
+                    int y = curr.second + dir.second;
                     if(x >= 0 && x < m && y >= 0 && y < n && maze[x][y] == '.') {
                         if(x == 0 || x == m - 1 || y == 0 || y == n - 1) return ans + 1;
                         maze[x][y] = '+';
