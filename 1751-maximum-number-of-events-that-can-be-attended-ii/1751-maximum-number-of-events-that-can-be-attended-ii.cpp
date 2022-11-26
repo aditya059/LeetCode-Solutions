c39819e2e48aa1d3ebb905b52781c92969db4bfb
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-11-26 21:55:08 +0530

    Time: 672 ms (44.06%), Space: 57.8 MB (95.34%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -17,6 +17,39 @@ public:
         int n = events.size();
         sort(events.begin(), events.end());
         vector<vector<int>> DP(k, vector<int>(n + 1, 0));
+        vector<int> index(n + 1);
+        for(int i = n - 1; i >= 0; i--) {
+            DP[0][i] = max(DP[0][i + 1], events[i][2]);
+            index[i] = binary_search(events, i + 1, n - 1, events[i][1] + 1);
+        }
+        for(int j = 1; j < k; j++) {
+            for(int i = n - 1; i >= 0; i--) {
+                DP[j][i] = max(DP[j][i + 1], events[i][2] + DP[j - 1][index[i]]);
+            }
+        }
+        return DP[k - 1][0];
+    }
+};
+/*
+class Solution {
+public:
+    int binary_search(vector<vector<int>> &intervals, int l, int h, int target) {
+        int ans = h + 1;
+        while(l <= h) {
+            int m = l + (h - l) / 2;
+            if(target > intervals[m][0]) {
+                l = m + 1;
+            } else {
+                ans = min(ans, m);
+                h = m - 1;
+            }
+        }
+        return ans;
+    }
+    int maxValue(vector<vector<int>>& events, int k) {
+        int n = events.size();
+        sort(events.begin(), events.end());
+        vector<vector<int>> DP(k, vector<int>(n + 1, 0));
         for(int i = n - 1; i >= 0; i--)
             DP[0][i] = max(DP[0][i + 1], events[i][2]);
         for(int j = 1; j < k; j++) {
@@ -28,3 +61,4 @@ public:
         return DP[k - 1][0];
     }
 };
+*/
