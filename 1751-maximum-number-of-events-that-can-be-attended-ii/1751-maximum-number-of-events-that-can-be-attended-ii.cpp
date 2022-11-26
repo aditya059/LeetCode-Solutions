22576378016754a661697885ff314ab2a0ba3e38
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-11-26 21:58:45 +0530

    Time: 617 ms (59.84%), Space: 52.2 MB (98.95%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -16,7 +16,7 @@ public:
     int maxValue(vector<vector<int>>& events, int k) {
         int n = events.size();
         sort(events.begin(), events.end());
-        vector<vector<int>> DP(k, vector<int>(n + 1, 0));
+        vector<vector<int>> DP(2, vector<int>(n + 1, 0));
         vector<int> index(n + 1);
         for(int i = n - 1; i >= 0; i--) {
             DP[0][i] = max(DP[0][i + 1], events[i][2]);
@@ -24,10 +24,10 @@ public:
         }
         for(int i = 1; i < k; i++) {
             for(int j = n - 1; j >= 0; j--) {
-                DP[i][j] = max(DP[i][j + 1], events[j][2] + DP[i - 1][index[j]]);
+                DP[i % 2][j] = max(DP[i % 2][j + 1], events[j][2] + DP[(i - 1) % 2][index[j]]);
             }
         }
-        return DP[k - 1][0];
+        return DP[(k - 1) % 2][0];
     }
 };
 /*
