240897cd43f40ca9983595cf326502b0372759da
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-11-27 13:53:34 +0530

    Time: 18 ms (94.93%), Space: 10.7 MB (35.61%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,3 +1,5 @@
+/*
+// Brute Force O(n^2)
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
@@ -15,21 +17,37 @@ public:
         return ans;
     }
 };
+*/
 
-/*const int MOD = 1e9 + 7;
-        long ans = 0;
-        int n = arr.size();
-        stack<int> Stack;
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        long long ans = 0;
+        int n = nums.size();
+        stack<int> StackI;  // Increasing Stack
+        stack<int> StackD;  // Decreasing Stack
         for(int i = 0; i <= n; i++) {
-            while(!Stack.empty() && (i == n || arr[i] <= arr[Stack.top()])) {
-                int middle = Stack.top();
-                Stack.pop();
+            while(!StackI.empty() && (i == n || nums[i] <= nums[StackI.top()])) {
+                int middle = StackI.top();
+                StackI.pop();
                 int right = i;
-                int left = Stack.empty()? -1: Stack.top();
-                long count = (middle - left) * (right - middle);
-                ans = (ans + (arr[middle] * count % MOD) % MOD) % MOD;
+                int left = StackI.empty()? -1: StackI.top();
+                long long count = (middle - left) * (right - middle);
+                ans = ans - nums[middle] * count;
             }
-            Stack.push(i);
+            StackI.push(i);
+            while(!StackD.empty() && (i == n || nums[i] >= nums[StackD.top()])) {
+                int middle = StackD.top();
+                StackD.pop();
+                int right = i;
+                int left = StackD.empty()? -1: StackD.top();
+                long long count = (middle - left) * (right - middle);
+                ans = ans + nums[middle] * count;
+            }
+            StackD.push(i);
         }
         return ans;
-        */
+    }
+};
+
+    
