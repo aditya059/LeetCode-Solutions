64feebd5c717935eb9afb44cc7f8a58a79efd0aa
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-12-02 21:48:28 +0530

    Time: 333 ms (9.09%), Space: 16.4 MB (45.45%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.go
+++ 1701-average-waiting-time/1701-average-waiting-time.go
@@ -1,11 +1,11 @@
 func averageWaitingTime(customers [][]int) float64 {
-    sum := 0.0;
-    end := float64(customers[0][0]);
+    var sum int = 0;
+    var end int = customers[0][0];
     for _, customer := range customers {
-        end = math.Max(end + float64(customer[1]), float64(customer[0]) + float64(customer[1]));
-        sum += end - float64(customer[0]);
+        end = int(math.Max(float64(end + customer[1]), float64(customer[0] + customer[1])));
+        sum += end - customer[0];
     }
-    return sum / float64(len(customers));
+    return float64(sum) / float64(len(customers));
 }
 
 
