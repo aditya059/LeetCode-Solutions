42e6201109be3e98a4bcc25e7ada9e518b74a9a4
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-12-03 01:17:51 +0530

    Time: 149 ms (83.33%), Space: 40.4 MB (66.67%) - LeetHub

--- 1702-maximum-binary-string-after-change/1702-maximum-binary-string-after-change.cpp
+++ 1702-maximum-binary-string-after-change/1702-maximum-binary-string-after-change.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     string maximumBinaryString(string binary) {
-        int count = 0, j;
-        for(int i = 0; i < binary.size(); i++) {
+        int count = 0, j = -1;
+        for(int i = binary.size() - 1; i >= 0; i--) {
             if(binary[i] == '0') {
+                count++;
                 j = i;
-                break;
             }
             binary[i] = '1';
         }
-        binary[j] = '1';
-        for(int i = j + 1; i < binary.size(); i++) {
-            if(binary[i] == '0') count++;
-            binary[i] = '1';
-        }
-        binary[j + count] = '0';
+        if(j != -1)
+            binary[j + count - 1] = '0';
         return binary;
     }
 };
