5e619eaddadb38f05a580d0943aa2c369afadf4a
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-12-05 02:54:54 +0530

    Time: 111 ms (31.48%), Space: 9.3 MB (39.84%) - LeetHub

--- 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.cpp
+++ 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.cpp
@@ -8,10 +8,13 @@ public:
             count[Map[ch]]++;
             if(ch == 'c') 
                 ans = max(ans, ++frog);
-            else if(--count[Map[ch] - 1] < 0)
-                return -1;
             else if(ch == 'k')
                 frog--;
+            else {
+                --count[Map[ch] - 1];
+                if(count[Map[ch] - 1] < 0)
+                    return -1;
+            }
         }
         return frog == 0? ans: -1;
     }
