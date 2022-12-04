bffe9f22e6977a070f3d91d7e2c594ebe51b4cb5
Aditya Jain <31334397+aditya059@users.noreply.github.com>
2022-12-04 09:57:58 +0530

    Time: 334 ms (31.06%), Space: 78.3 MB (76.81%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int minimumAverageDifference(vector<int>& nums) {
-        int n = nums.size();
         long long sum = accumulate(nums.begin(), nums.end(), 0L);
+        int n = nums.size();
         int avg;
         int ans = 0;
         int minAvg = INT_MAX;
         long long currSum = nums[0];
         for(int i = 1; i < n; i++) {
-            avg = (currSum / i) - (sum - currSum) / (nums.size() - i);
+            avg = (currSum / i) - (sum - currSum) / (n - i);
             avg = abs(avg);
             currSum += nums[i];
             if(avg < minAvg) {
