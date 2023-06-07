f755924d4a33aa035b620f931b9d35af2b74b155
Aditya Jain <adi.jain059@gmail.com>
2023-06-07 10:30:16 +0530

    Update 1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -3,9 +3,9 @@ public:
     int minFlips(int a, int b, int c) {
         int ans = 0;
         for(int i = 0; i < 32; i++) {
-            bool A = (1 << i) & a;
-            bool B = (1 << i) & b;
-            bool C = (1 << i) & c;
+            bool A = (a >> i) & 1;
+            bool B = (b >> i) & 1;
+            bool C = (c >> i) & 1;
             if(A && B && !C) ans += 2;
             else if(C && (A || B)) continue;
             else if(!A && !B && !C) continue;
