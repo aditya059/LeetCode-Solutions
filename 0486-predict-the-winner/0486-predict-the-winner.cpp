836659768283e19c2bca1a99c9d62e7a3b5084a9
Aditya Jain <adi.jain059@gmail.com>
2023-07-28 14:06:18 +0530

    Update 0486-predict-the-winner.cpp

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,3 +1,4 @@
+// Recursion
 class Solution {
 public:
     int solve(int i, int j, vector<int> &nums) {
@@ -6,5 +7,19 @@ public:
     }
     bool PredictTheWinner(vector<int>& nums) {
         return solve(0, nums.size() - 1, nums) >= 0;
+    }
+};
+
+// DP
+class Solution {
+public:
+    int solve(int i, int j, vector<int> &nums, vector<vector<int>> &memo) {
+        if(i == j) return nums[i];
+        if(memo[i][j] != -1) return memo[i][j];
+        return memo[i][j] = max(nums[i] - solve(i + 1, j, nums, memo), nums[j] - solve(i, j - 1, nums, memo));
+    }
+    bool PredictTheWinner(vector<int>& nums) {
+        vector<vector<int>> memo(nums.size(), vector<int>(nums.size(), -1));
+        return solve(0, nums.size() - 1, nums, memo) >= 0;
     }
 };
