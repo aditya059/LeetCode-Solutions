c5bfe5e4ccdd169d2ffefb9b1ef0a6e126ebf9d2
Aditya Jain <adi.jain059@gmail.com>
2023-12-03 14:01:19 +0530

    Update 1266-minimum-time-visiting-all-points.cpp

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -11,3 +11,21 @@ public:
         return ans;
     }
 };
+
+
+OR
+
+
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        int ans = 0;
+        for(int i = 1; i < n; i++) {
+            int diffX = abs(points[i][0] - points[i - 1][0]);
+            int diffY = abs(points[i][1] - points[i - 1][1]);
+            ans += max(diffX, diffY);
+        }
+        return ans;
+    }
+};
