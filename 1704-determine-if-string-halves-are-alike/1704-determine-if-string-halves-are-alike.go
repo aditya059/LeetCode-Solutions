a50f3951bab97aae17e715038eb466bc16794cac
Aditya Jain <adi.jain059@gmail.com>
2024-01-12 12:40:01 +0530

    Update 1704-determine-if-string-halves-are-alike.go

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.go
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.go
@@ -11,3 +11,20 @@ func numOfVowels(s string) int {
 func halvesAreAlike(s string) bool {
     return numOfVowels(s[:len(s) / 2]) == numOfVowels(s[len(s)/2:])
 }
+
+OR
+
+class Solution {
+    bool isVowel(char ch) {
+        ch = tolower(ch);
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+public:
+    bool halvesAreAlike(string s) {
+        int count = 0;
+        for(int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            count += isVowel(s[i]) - isVowel(s[j]);
+        }
+        return count == 0;
+    }
+};
