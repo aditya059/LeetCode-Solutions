a1b81429aaaf631dacacf655b05b7776a6cd49ff
Aditya Jain <adi.jain059@gmail.com>
2024-05-17 08:20:53 +0530

    Update 1325-delete-leaves-with-a-given-value.cpp

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
@@ -12,11 +12,10 @@
 class Solution {
 public:
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
-        if(!root) return NULL;
+        if(!root) return root;
         root -> left = removeLeafNodes(root -> left, target);
         root -> right = removeLeafNodes(root -> right, target);
-        if(!root -> left && !root -> right && root -> val == target) 
-            return NULL;
+        if(!root -> left && !root -> right && root -> val == target) return nullptr; 
         return root;
     }
 };
