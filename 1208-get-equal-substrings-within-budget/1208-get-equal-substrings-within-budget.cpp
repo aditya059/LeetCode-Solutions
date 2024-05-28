f9ad8a9015819d13af5fa62c73dde8bb150b9499
Aditya Jain <adi.jain059@gmail.com>
2024-05-28 07:03:51 +0530

    Update 1208-get-equal-substrings-within-budget.cpp

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     int equalSubstring(string s, string t, int maxCost) {
         int ans = 0;
-        int start = 0;
-        int end = 0;
-        int cost = 0;
-        while(end < s.length()) {
-            cost += abs(s[end] - t[end]);
-            while(cost > maxCost) {
-                cost -= abs(s[start] - t[start]);
+        for(int start = 0, end = 0; end < s.length(); end++) {
+            maxCost -= abs(s[end] - t[end]);
+            while(maxCost < 0) {
+                maxCost += abs(s[start] - t[start]);
                 start++;
             }
-            end++;
-            ans = max(ans, end - start);
+            ans = max(ans, end - start + 1);
         }
         return ans;
     }
