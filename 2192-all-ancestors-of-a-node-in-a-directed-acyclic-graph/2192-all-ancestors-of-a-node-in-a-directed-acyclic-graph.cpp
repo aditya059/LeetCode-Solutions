e4a7603433b4f9cec87d5955c10d05ad306a3e5c
Aditya Jain <adi.jain059@gmail.com>
2024-06-29 12:11:10 +0530

    Update 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -26,3 +26,31 @@ public:
         return ans;
     }
 };
+
+OR
+
+class Solution {
+public:
+	void dfs(int curr, int &val, vector<int> adjList[], vector<bool> &visited, vector<vector<int>> &ans) {
+		if(visited[curr]) return;
+		visited[curr] = true;
+		for(int next: adjList[curr]) {
+			if(!visited[next]) {
+				ans[next].push_back(val);
+				dfs(next, val, adjList, visited, ans);
+			}
+		}
+	}
+	vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+		vector<vector<int>> ans(n);
+		vector<int> adjList[n];
+		for(vector<int> edge: edges) {
+			adjList[edge[0]].push_back(edge[1]);
+		}
+		for(int i = 0; i < n; i++) {
+			vector<bool> visited(n);
+			dfs(i, i, adjList, visited, ans);
+		}
+		return ans;
+	}
+};
