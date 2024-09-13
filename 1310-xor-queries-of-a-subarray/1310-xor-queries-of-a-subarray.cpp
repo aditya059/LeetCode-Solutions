091fca6c832fff322f565f2832b70dd27e9d6cd2
Aditya Jain <adi.jain059@gmail.com>
2024-09-13 12:03:15 +0530

    Update 1310-xor-queries-of-a-subarray.cpp

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -14,3 +14,21 @@ public:
         return ans;
     }
 };
+
+OR
+
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        int n = arr.size();
+        vector<int> prefixXorred(n + 1);
+        for(int i = 1; i <= n; i++) {
+            prefixXorred[i] = prefixXorred[i - 1] xor arr[i - 1];
+        }
+        vector<int> ans;
+        for(int i = 0; i < queries.size(); i++) {
+            ans.push_back(prefixXorred[queries[i][1] + 1] xor prefixXorred[queries[i][0]]);
+        }
+        return ans;
+    }
+};
