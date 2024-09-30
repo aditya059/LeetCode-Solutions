c70def577a49b27b3ffe39677c858f26cae4f231
Aditya Jain <adi.jain059@gmail.com>
2024-09-30 12:40:35 +0530

    Update 1381-design-a-stack-with-increment-operation.cpp

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
@@ -36,3 +36,45 @@ public:
  * int param_2 = obj->pop();
  * obj->increment(k,val);
  */
+
+OR
+
+class CustomStack {
+public:
+    vector<int> Stack, incr;
+    int maxSize;
+    CustomStack(int maxSize) {
+        this -> maxSize = maxSize;
+    }
+    
+    void push(int x) {
+        if(Stack.size() >= maxSize)
+            return;
+        Stack.push_back(x);
+        incr.push_back(0);
+    }
+    
+    int pop() {
+        if(Stack.empty()) return -1;
+        int ans = Stack.back() + incr.back();
+        Stack.pop_back();
+        int val = incr.back();
+        incr.pop_back();
+        if(incr.size()) incr.back() += val;
+        return ans;
+    }
+    
+    void increment(int k, int val) {
+        k = min(k, (int)incr.size());
+        if(k)
+            incr[k - 1] += val;
+    }
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
