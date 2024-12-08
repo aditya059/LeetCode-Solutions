0c16d8364f5285879de7ded95c4bb70dc9f57aa0
Aditya Jain <adi.jain059@gmail.com>
2024-12-08 12:44:52 +0530

    Update 2054-two-best-non-overlapping-events.cpp

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -17,3 +17,26 @@ public:
         return ans;
     }
 };
+
+OR
+
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& events) {
+        sort(events.begin(), events.end());
+        int n = events.size(), maxVal = 0, ans = 0;
+        auto comp = [&](int &i, int &j) {
+            return events[i][1] > events[j][1];
+        };
+        priority_queue<int, vector<int>, decltype(comp)> PQ(comp);
+        for(int i = 0; i < n; i++) {
+            while(!PQ.empty() && events[PQ.top()][1] < events[i][0]) {
+                maxVal = max(maxVal, events[PQ.top()][2]);
+                PQ.pop();
+            }
+            ans = max(ans, maxVal + events[i][2]);
+            PQ.push(i);
+        }
+        return ans;
+    }
+};
