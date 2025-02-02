c864c8bbf186d33109953495bae1c8a85d0e6023
Aditya Jain <adi.jain059@gmail.com>
2025-02-02 10:02:55 +0530

    Update 1752-check-if-array-is-sorted-and-rotated.cpp

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -13,3 +13,18 @@ public:
         return nums.size() <= i && nums[nums.size() - 1] <= nums[0];
     }
 };
+
+OR
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        int inversion = nums[0] < nums[n - 1];
+        for(int i = 1; i < n; i++) {
+            if(nums[i] < nums[i - 1]) inversion++;
+            if(inversion > 1) return false;
+        }
+        return true;
+    }
+};
